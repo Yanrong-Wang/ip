215c93832acf9a6f428f64fe9eaed142416c2fbd
“Yanrong-Wang” <e1485168@u.nus.edu>
2026-08-21 04:04:40 +0800

    Add Lizzy greeting and farewell

--- src/main/java/Lizzy.java
+++ src/main/java/Lizzy.java
@@ -3,17 +3,24 @@
  */
 public class Lizzy {
     /**
-     * Displays Lizzy's banner before ending the program.
+     * Displays Lizzy's greeting and farewell before ending the program.
      *
      * @param args command-line arguments, which are not used
      */
     public static void main(String[] args) {
+        String divider = "____________________________________________________________";
         String banner = "    __    _\n"
                 + "   / /   (_)_______  __  __\n"
                 + "  / /   / /_  /_  / / / / /\n"
                 + " / /___/ / / /_/ /_/ /_/ /\n"
                 + "/_____/_/ /___/___/\\__, /\n"
                 + "                  /____/\n";
+        System.out.println(divider);
         System.out.print(banner);
+        System.out.println("Hello! I'm Lizzy.");
+        System.out.println("What brings you here today?");
+        System.out.println(divider);
+        System.out.println("Bye! I hope our next conversation will be just as agreeable.");
+        System.out.println(divider);
     }
 }
